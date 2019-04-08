e116e6363c012357e4a4b880b0ad39d7d261cd0b
Jonathan Geiger <jonathan.geiger@student.hs-rm.de>
2019-04-08 13:08:33 +0200

    Initial Commit

---
+++ Counter-min/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter-min
+//
+//  Created by Jonathan Geiger on 08.04.19.
+//  Copyright © 2019 JonathanGeiger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
